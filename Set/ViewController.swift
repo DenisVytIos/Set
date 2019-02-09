dee8d65925f05d9aae55fe9dae6562c46a646d0e
DenisVytIos <denvitios@gmail.com>
2019-02-09 06:47:43 +0200

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Denis on 09.02.2019.
+//  Copyright © 2019 Denis Vitrishko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
